3e36632368fbb4b09a2aee8b2e62216e2d08ac46
xiplus <huangxuanyuxiplus@gmail.com>
2018-11-14 23:04:27 +0800

    Select algorithms automatically

--- compress.py
+++ compress.py
@@ -7,6 +7,7 @@ from huffman import *
 import time
 
 algolist = {
+	"auto": [],
 	"lz78": [LZ78_encode, LZ78_decode, []],
 	"xp01": [XIPLUS01_encode, XIPLUS01_decode, [True]],
 	"xp01b": [XIPLUS01_encode, XIPLUS01_decode, [False]],
@@ -28,7 +29,33 @@ if ctype == "encode":
 	oldsize = os.path.getsize(finname)
 	print("old size: {}".format(oldsize))
 	start = time.time()
-	algolist[algo][0](finname, foutname, *algolist[algo][2])
+	if algo == "auto":
+		minsize = 2**100
+		minalgo = ""
+		for algo2 in list(algolist.keys())[1:]:
+			fouttmp = "tmp_{}.bin".format(algo2)
+			result = algolist[algo2][0](finname, fouttmp, *algolist[algo2][2])
+			if result == False:
+				print("{} skip".format(algo2))
+				continue
+			newsize = os.path.getsize(fouttmp)
+			print("{} {}".format(algo2, newsize))
+			if newsize < minsize:
+				minalgo = algo2
+				minsize = newsize
+
+		print("real encode by {}".format(minalgo))
+		for algo2 in list(algolist.keys())[1:]:
+			fouttmp = "tmp_{}.bin".format(algo2)
+			if algo2 == minalgo:
+				os.replace(fouttmp, foutname)
+			else:
+				if os.path.exists(fouttmp):
+				   os.remove(fouttmp)
+		with open(foutname, "ab") as fout:
+			fout.write(bytes([list(algolist.keys()).index(minalgo)]))
+	else:
+		algolist[algo][0](finname, foutname, *algolist[algo][2])
 	print("spend {} s".format(time.time()-start))
 	# result = algolist[algo][0](data)
 	# print("new size: {}".format(len(result)))
@@ -46,7 +73,18 @@ elif ctype == "decode":
 	# 	data = fin.read()
 	# print("old size: {}".format(len(data)))
 	start = time.time()
-	algolist[algo][1](finname, foutname, *algolist[algo][2])
+	if algo == "auto":
+		with open(finname, 'rb') as fin:
+			data = fin.read()
+		fintmp = "tmp_auto.bin"
+		with open(fintmp, 'wb') as fin:
+			fin.write(data[:-1])
+		algo2 = list(algolist.keys())[data[-1]]
+		print("real decode by {}".format(algo2))
+		algolist[algo2][1](fintmp, foutname, *algolist[algo2][2])
+		os.remove(fintmp)
+	else:
+		algolist[algo][1](finname, foutname, *algolist[algo][2])
 	print("spend {} s".format(time.time()-start))
 	# result = algolist[algo][1](data)
 	# print("new size: {}".format(len(result)))
