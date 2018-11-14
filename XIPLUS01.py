bc89b4b5cf6d9b5945fbf15b1d4452a7bd9e957a
xiplus <huangxuanyuxiplus@gmail.com>
2018-11-14 21:52:28 +0800

    Support non-utf8 file in xp01

--- XIPLUS01.py
+++ XIPLUS01.py
@@ -1,29 +1,45 @@
 import math
 
-def XIPLUS01_encode(infile, outfile):
+
+''' file foramt
+	char list
+	content
+	4 bytes: header size
+'''
+
+def XIPLUS01_encode(infile, outfile, utf8):
+	print("\tutf8 =", utf8)
+
 	with open(infile, "rb") as fin:
 		data = fin.read()
 
-	data = data.decode()
-	with open(outfile, "wb") as fout:
-		# result = b""
+	if utf8:
+		data = data.decode()
 
+	with open(outfile, "wb") as fout:
 		chars = list(set(list(data)))
 		print("\tfind {} different chars".format(len(chars)))
+
+		headersize = 0
+		if utf8:
+			fout.write(("".join(chars)).encode())
+			headersize = len(("".join(chars)).encode())
+		else:
+			for char in chars:
+				fout.write(bytes([char]))
+				headersize += len(bytes([char]))
+
+		print("\theadersize =", headersize)
+
 		dic = {}
 		for t in range(len(chars)):
-			dic[chars[t]] = t+1
-		# print(chars)
-		# result += ("".join(chars)).encode()
-		# result += bytes([0])
-		fout.write(("".join(chars)).encode())
-		fout.write(bytes([0]))
+			dic[chars[t]] = t
 
 		cbit = math.ceil(math.log2(len(chars)))
-		# print("cbit=", cbit)
+		print("\tcbit =", cbit)
+
 		temp = ""
 		for char in data:
-			# temp += bin(chars.index(char) + 1)[2:].zfill(cbit)
 			temp += bin(dic[char])[2:].zfill(cbit)
 			while len(temp) >= 8:
 				fout.write(bytes([int(temp[0:8], 2)]))
@@ -35,34 +51,40 @@ def XIPLUS01_encode(infile, outfile):
 		while len(temp) >= 8:
 			fout.write(bytes([int(temp[0:8], 2)]))
 			temp = temp[8:]
-		# for i in range(0, len(temp), 8):
-		# 	result += bytes([int(temp[i:i+8], 2)])
+		fout.write(headersize.to_bytes(4, 'big'))
 
-	# return result
+	return True
 
-def XIPLUS01_decode(infile, outfile):
+def XIPLUS01_decode(infile, outfile, utf8):
+	print("\tutf8 =", utf8)
+
 	with open(infile, "rb") as fin:
 		data = fin.read()
 
 	with open(outfile, "wb") as fout:
-		i = 0
-		while data[i] != 0:
-			i += 1
-		chars = data[:i]
-		# print(chars.decode())
-		chars = list(chars.decode())
+		headersize = int.from_bytes(data[-4:], "big")
+		print("\theadersize =", headersize)
 
-		# print(chars)
+		chars = data[0:headersize]
+
+		if utf8:
+			chars = list(chars.decode())
+
 		cbit = math.ceil(math.log2(len(chars)))
-		# print("cbit=", cbit)
+		print("\tcbit=", cbit)
 
 		temp = ""
-		for b in data[i+1:]:
-			temp += bin(b)[2:].zfill(8)
+		offset = headersize
+		datalen = len(data)
+		while offset < datalen - 4:
+			temp += bin(data[offset])[2:].zfill(8)
+			while len(temp) > cbit:
+				if utf8:
+					fout.write(chars[int(temp[0:cbit], 2)].encode())
+				else:
+					fout.write(bytes([chars[int(temp[0:cbit], 2)]]))
+				temp = temp[cbit:]
 
-		# result = b""
-		for i in range(cbit, len(temp), cbit):
-			fout.write(chars[int(temp[i-cbit:i], 2)-1].encode())
-			# result += chars[int(temp[i-cbit:i], 2)-1].encode()
+			offset += 1
 
-	# return result
+	return True
